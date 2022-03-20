ee44251e13ad8369db262af5871e7a65e14496ed
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-20 22:37:26 +0530

    Another data type, list

--- hello.py
+++ hello.py
@@ -13,23 +13,47 @@
 
 ## String Formatter
 ## First String Formating Method.
-x = "Hello..!!"
-y = "My Name Is"
-z = "Alex And My Age Is:"
-a = 20
+# x = "Hello..!!"
+# y = "My Name Is"
+# z = "Alex And My Age Is:"
+# a = 20
 
-e = x + " " + y + " " + z
-e = "My Name Is {} {}".format(z,a) ## We have used format method because considering that users name is not provided and is being entered dynamically. Here {} are used to represent the value of z. There can be more than one {}.
+# e = x + " " + y + " " + z
+# e = "My Name Is {} {}".format(z,a) ## We have used format method because considering that users name is not provided and is being entered dynamically. Here {} are used to represent the value of z. There can be more than one {}.
 
 # print(e)
 
 ## Second String Formatting Method.
 
-e = f"My name is {z} {a}" ## Here we have used f in the starting which is used for formatting the string according to our need.
+# e = f"My name is {z} {a}" ## Here we have used f in the starting which is used for formatting the string according to our need.
 # print(e)
 
 ## String Formatting Using List
 
-hey = ["Dev", "Sports", "20"]
-e = f"My name is {hey[0]}, my hobby is {hey[1]} and my age is {hey[2]}"
-print(e)
+# hey = ["Dev", "Sports", "20"]
+# e = f"My name is {hey[0]}, my hobby is {hey[1]} and my age is {hey[2]}"
+# print(e)
+
+## String Formatting Ended
+
+## List Data Type Starts
+
+## --> List is simply defined as the array of strings, numbers, etc.
+
+# print(dir(list)) ## This will shows how many methods we can use with list
+
+# x = ["Hello", "There", "How", "Are", "You", 8, 10.6]
+# print("Length Of List x is: ", len(x)) ## Prints the length of the list
+
+# x.pop() ## Here last element if removed
+# x.append(11) ## Here element is added at last position
+# x.remove("There") ## Here selected element is removed from the list
+# del x ## Deletes the list
+# print(x)
+
+## Likewise there are other methods in list
+
+## List Data Type Ended
+
+
+
