2e50d067c10e35b1370e84d9c86b68cb4c974240
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-20 19:12:19 +0530

    Commited work uptil now which includes variables, data types, operators and like that

---
+++ hello.py
@@ -0,0 +1,15 @@
+# print('Hello')
+# x = 2
+# y = float(x)
+# print(type(y))
+# print(dir("strings"))
+# print(" ")
+# print(dir("int"))
+# print(" ")
+# print(dir("float"))
+
+from operator import indexOf
+
+
+x = "water"
+print(x.indexOf("e"))
