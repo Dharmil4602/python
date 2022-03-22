44d2e3c56dd646798d9cca3983d0107df07888ff
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-22 22:44:58 +0530

    Completed Arithmetic Operators

---
+++ operators.py
@@ -0,0 +1,17 @@
+## Types Of Operators
+## 1. Arithmetic Operators
+## 2. Assignment Operators
+## 3. Comparison Operators
+## 4. Logical Operators
+## 5. Identity Operators
+## 6. Membership Operators
+## 7. Bitwise Operators
+
+## 1. Arithmetic Operators
+print(4+5) ## Addition
+print(5-4) ## Subtraction
+print(10/5) ## Division --> Division operator is used for finding the quotient
+print(4*5) ## Multiplication
+print(25%2) ## Modulo --> Modulo operator is used for finding the remainder
+print(2**4) ## Power Operator
+print(9//2) ## Same as division operator but here the value gets rounded off (We get only integer values here)
