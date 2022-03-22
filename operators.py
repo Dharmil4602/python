a7cec3275e272f2aa69303b7e417750689e083cc
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-22 23:06:39 +0530

    Completed Assignment Operators

--- operators.py
+++ operators.py
@@ -8,10 +8,29 @@
 ## 7. Bitwise Operators
 
 ## 1. Arithmetic Operators
-print(4+5) ## Addition
-print(5-4) ## Subtraction
-print(10/5) ## Division --> Division operator is used for finding the quotient
-print(4*5) ## Multiplication
-print(25%2) ## Modulo --> Modulo operator is used for finding the remainder
-print(2**4) ## Power Operator
-print(9//2) ## Same as division operator but here the value gets rounded off (We get only integer values here)
+
+# print(4+5) ## Addition
+# print(5-4) ## Subtraction
+# print(10/5) ## Division --> Division operator is used for finding the quotient
+# print(4*5) ## Multiplication
+# print(25%2) ## Modulo --> Modulo operator is used for finding the remainder
+# print(2**4) ## Power Operator
+# print(9//2) ## Same as division operator but here the value gets rounded off (We get only integer values here)
+
+
+## 2. Assignment Operator
+
+x = 2 ## Equals Operator
+print(x)
+x += 4 ## This is same as x = x + 4
+print(x)
+x -= 2 ## This is same as x = x - 2
+print(x)
+x *= 2 ## This Is Same As x = x * 2
+print(x)
+x /= 2 ## This Is Same As x = x / 2
+print(x)
+x %= 2 ## This Is Same As x = x % 2
+print(x)
+x **= 2 ## This Is Same As x = x ** 2
+print(x)
