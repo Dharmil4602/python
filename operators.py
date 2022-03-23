9ca83462cf214f8f423fe8ea2f3d34d94b551b21
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-23 20:58:30 +0530

    Identity Operator and Membership Operator

--- operators.py
+++ operators.py
@@ -43,3 +43,30 @@
 # 4. Greater Than Equal To Operator (>=)
 # 5. Less Than Operator (<)
 # 6. Less Than Equal To Operator (<=)
+
+## Identity Operators
+
+# 1. is
+# 2. is not
+# x = 3
+# y = 3
+# a = 1
+# b = 2
+# print(x is y)
+# print(a is not b)
+
+## Membership Operators
+
+# 1. in ## Specifies whether the particular element is present in the list, tuple. If not present then it will return false
+# 2. not in ## Specifies whether the particular element is not present in the list, tuple. If not present then it will return true
+
+x = [1,2,3,4]
+print(2 in x)
+print(5 not in x)
+print(4 not in x)
+
+## Similarly we can check it for strings too.
+
+a = "Hey There"
+print("e" in a)
+print("T" not in a)
