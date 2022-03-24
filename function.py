54392383e87f46259647ed7749011d2a3fff4614
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-24 10:14:57 +0530

    User Input

---
+++ function.py
@@ -0,0 +1,8 @@
+## User Input
+x = input("Enter The First Number: ")
+y = input("Enter The Second Number: ")
+## We need to specify what type of input we are giving, otherwise it will by default concatenate the input
+
+z = int(x) + int(y)
+# z = x + y
+print(z)
