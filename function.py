ff2661a07e9ffad905e0853fd191b7f6068707bb
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-24 10:20:55 +0530

    Temperature Convertor Example

--- function.py
+++ function.py
@@ -1,8 +1,16 @@
 ## User Input
-x = input("Enter The First Number: ")
-y = input("Enter The Second Number: ")
-## We need to specify what type of input we are giving, otherwise it will by default concatenate the input
+# x = input("Enter The First Number: ")
+# y = input("Enter The Second Number: ")
+# ## We need to specify what type of input we are giving, otherwise it will by default concatenate the input
 
-z = int(x) + int(y)
-# z = x + y
-print(z)
+# z = int(x) + int(y)
+# # z = x + y
+# print(z)
+
+
+## Temperature Convertor
+
+c = input("Enter The Temperature: ")
+c = int(c)
+f = (9/5)*c + 32
+print(f)
