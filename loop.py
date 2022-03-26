cfafb5aad85b1f2e95b54d204043582863679c48
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-27 02:09:46 +0530

    For in loop

---
+++ loop.py
@@ -0,0 +1,13 @@
+## For loop
+x = [0,1,2,3,4,5,6,7,8,9,10]
+
+# for i in x:
+#     print(i)
+    
+
+## Taking input from the user using for in loop
+
+for i in x:
+    x[i] = input("Enter The Name: ")
+
+print(x)
