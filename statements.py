471290818e0280d4ed8c665891f173efd9dd8ad4
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-27 23:37:48 +0530

    statements done

---
+++ statements.py
@@ -0,0 +1,15 @@
+## Break Statement
+
+# x = ["Yellow", "Blue", "Green", "Black", "Red", "While"]
+# for i in x:
+#     if i == "Black":
+#         break
+#     print(i) ## This will print the details before Black
+
+## Continue Statement
+
+x = ["Yellow", "Blue", "Green", "Black", "Red", "While"]
+for i in x:
+    if i == "Black":
+        continue ## continue will skip the condition mentioned. in this case Black will be skipped
+    print(i) 
