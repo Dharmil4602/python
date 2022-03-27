e4eaa2dd857ab419f69118d447c2149923c5609c
Dharmil Shah <dharmilshah476@gmail.com>
2022-03-27 23:18:45 +0530

    Loops done

--- loop.py
+++ loop.py
@@ -1,5 +1,5 @@
 ## For loop
-x = [0,1,2,3,4,5,6,7,8,9,10]
+# x = [0,1,2,3,4,5,6,7,8,9,10]
 
 # for i in x:
 #     print(i)
@@ -7,7 +7,18 @@ x = [0,1,2,3,4,5,6,7,8,9,10]
 
 ## Taking input from the user using for in loop
 # Note: here the list should be starting from zero incase the list includes numbers.
-for i in x:
-    x[i] = input("Enter The Name: ")
+# for i in x:
+#     x[i] = input("Enter The Name: ")
+# print(x)
 
-print(x)
+## While loop
+
+# x = 0
+# while x<10:
+#     print(x)
+#     x += 1
+
+## Looping through the string
+x = "Hello"
+for i in x:
+    print(i)
