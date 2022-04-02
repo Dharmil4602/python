23c23fc5e5386aa35da6e10f975e84dd4a775b80
Dharmil Shah <dharmilshah476@gmail.com>
2022-04-02 18:42:38 +0530

    Date Time completed

---
+++ date-time.py
@@ -0,0 +1,13 @@
+import datetime
+a = datetime
+# x = a.datetime.now()
+x = datetime.datetime.now()
+print(x)
+print(x.year)
+print(x.month)
+print(x.hour)
+print(x.minute)
+print(x.second)
+print(x.day)
+
+## Search about strftime() on google where you will find the whole list of strftime()
