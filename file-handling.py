42e66e62de14242198171b1c1d70383db95de539
Dharmil Shah <dharmilshah476@gmail.com>
2022-04-04 22:25:57 +0530

    Append completed

--- file-handling.py
+++ file-handling.py
@@ -3,9 +3,10 @@
 ## First we need to open the file with the help of open() method in which we need to pass two parameters. open("file name", "the mode in which we need to open it")
 
 ## Mode of opening the files can be read mode (r), write mode (w), append mode (a).
-openFile = open("test.txt", "r") ## Read mode
-x = openFile.read()
-print(x)
+
+# openFile = open("test.txt", "r") ## Read mode
+# x = openFile.read()
+# print(x)
 
 ## While we are in reading mode, we need not to close the file.
 ## But when we are in writing mode we need to close the file.  
@@ -13,3 +14,9 @@ print(x)
 # writeFile = open("test.txt", "a")
 # writeFile.write( "Orange\n")
 # writeFile.close()
+
+## Append Method
+
+# file = open("test.txt", "w")
+# file.write("Hello")
+# file.close()
