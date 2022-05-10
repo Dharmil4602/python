c90ab30a2770714ee64f27c40d5fbfc20e2f484e
Dharmil Shah <dharmilshah476@gmail.com>
2022-05-10 22:26:59 +0530

    Reversing The Name

---
+++ Python Problems/problem-4.py
@@ -0,0 +1,7 @@
+fname = input("Enter Your First Name: \n")
+lname = input("Enter Your Last Name: \n")
+x = fname + " " + lname
+print(x)
+print("After Reversing The Order: \n")
+y = lname + " " + fname
+print(y)
