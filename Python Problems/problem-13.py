aab34ece8da6042d031e75d271f40edca85b5329
Dharmil Shah <dharmilshah476@gmail.com>
2022-06-29 02:43:05 +0530

    Completed 13th problem

---
+++ Python Problems/problem-13.py
@@ -0,0 +1,8 @@
+import string
+
+
+str = input("Enter a character: ")
+if(str == "a" or str == "e" or str == "i" or str == "o" or str == "u" or str == "A" or str == "E" or str == "I" or str == "O" or str == "U"):
+    print("Vowel")
+else:
+    print("Consonant")
