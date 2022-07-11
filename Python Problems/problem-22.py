e8780bd8f2b9d2974f8da85ab4096208efc1ca8a
Dharmil Shah <dharmilshah476@gmail.com>
2022-07-11 16:56:23 +0530

    Added automated python script

---
+++ Python Problems/problem-22.py
@@ -0,0 +1,9 @@
+# Python Automated Script
+
+import pyautogui as pg
+import time
+print("Starting in 10 seconds...")
+time.sleep(10)
+for i in range(50):
+    pg.write("Aaje Javanu K Nai AECC???")
+    pg.press("Enter")
