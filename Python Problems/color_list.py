e719b49eb5362c35c9d07894b0bd1d032aed30ba
Dharmil Shah <dharmilshah476@gmail.com>
2022-08-02 11:57:42 +0530

    Printed difference of colors

--- Python Problems/color_list.py
+++ Python Problems/color_list.py
@@ -1,1 +1,6 @@
 # Write a Python program to print out a set containing all the colors from color_list_1 which are not present in color_list_2
+color_list_1 = set(["Red", "Green", "White", "Black"])
+color_list_2 = set(["Green", "Black"])
+# for i,j in color_list_1,color_list_2:
+#     if color_list_1[i] != color_list_2[j]:
+print(color_list_1.difference(color_list_2))
