87b589678b6967dbf303287225532cfe0439c4b8
Dharmil Shah <dharmilshah476@gmail.com>
2022-09-19 00:35:43 +0530

    Added files

---
+++ Python Problems/numbers_different_from_other.py
@@ -0,0 +1,6 @@
+# Write a Python function that takes a sequence of numbers and determines whether all the numbers are different from each other
+enterNumbers = int(input("Enter Few Numbers \n"))
+number_list = []
+for i in range(enterNumbers):
+    number_list.append(i) 
+print(number_list)
